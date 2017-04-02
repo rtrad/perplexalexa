4b80ecf6768cf71ddd6734ecacd120e3fd349f31
rtrad3 <trad.ryan.alan@gmail.com>
2017-04-01 21:01:46 -0400

    added length param to response api

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask, json
+from flask import Flask, json, request
 import perplex
 
 app = Flask(__name__)
 
 @app.route('/respond/<string:sentence>', methods = ['GET'])
-def get_response(sentence):
-    return json.dumps(perplex.get_response(sentence))
+def get_response(sentence, length=8):
+    if 'length' in request.args:
+        length = int(request.args.get('length').strip())
+    return json.dumps(perplex.get_response(sentence, length=length))
 
 if __name__ == '__main__':
     app.run(debug=True)
